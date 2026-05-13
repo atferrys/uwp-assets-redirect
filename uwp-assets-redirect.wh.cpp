0d5bb2c465561e71b60bea9d1ec1ee67791ab6a9
atferrys <61624086+atferrys@users.noreply.github.com>
2026-05-13 21:22:09 +0200

    chore: Bump version to 1.1

--- uwp-assets-redirect.wh.cpp
+++ uwp-assets-redirect.wh.cpp
@@ -2,7 +2,7 @@
 // @id              uwp-assets-redirect
 // @name            UWP Assets Redirect
 // @description     Replace UWP app assets (such as icons) without worrying about updates or modifying system files permissions.
-// @version         1.0
+// @version         1.1
 // @author          ferrys
 // @github          https://github.com/atferrys
 // @license         GPL-3.0
